aed76ba4118901f6d0fc695d78d842d394d1e2cb
Ehsan Nasiri <ehsannas@gmail.com>
2021-11-19 15:15:15 -0600

    Make the Filter class abstract.

--- firebase-firestore/src/main/java/com/google/firebase/firestore/Filter.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/Filter.java
@@ -1,15 +1,15 @@
 package com.google.firebase.firestore;
 
 import androidx.annotation.NonNull;
-
 import com.google.firebase.firestore.model.Document;
 
-public class Filter {
+public abstract class Filter {
   @NonNull
   public static Filter equalTo(@NonNull FieldPath fieldPath, @NonNull Object value) {
     return null;
   }
 
-  public boolean matches(Document doc) { return false; }
-  public String getCanonicalId() { return ""; }
+  public abstract boolean matches(Document doc);
+
+  public abstract String getCanonicalId();
 }
