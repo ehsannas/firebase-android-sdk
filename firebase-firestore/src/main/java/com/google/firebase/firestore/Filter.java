f3db1321e8acf2da3839341cd74fe4d763a7f2df
Ehsan Nasiri <ehsannas@gmail.com>
2021-11-24 13:50:05 -0600

    Add the other filter APIs.

--- firebase-firestore/src/main/java/com/google/firebase/firestore/Filter.java
+++ firebase-firestore/src/main/java/com/google/firebase/firestore/Filter.java
@@ -1,6 +1,7 @@
 package com.google.firebase.firestore;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import com.google.firebase.firestore.core.CompositeFilter;
 import com.google.firebase.firestore.core.FieldFilter;
 import com.google.firebase.firestore.core.UnqualifiedFieldFilter;
@@ -9,15 +10,113 @@ import java.util.Arrays;
 
 public abstract class Filter {
   @NonNull
-  public static Filter equalTo(@NonNull String field, @NonNull Object value) {
-    return new UnqualifiedFieldFilter(
-        FieldPath.fromDotSeparatedPath(field).getInternalPath(), FieldFilter.Operator.EQUAL, value);
+  public static Filter equalTo(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.EQUAL, value);
   }
 
   @NonNull
-  public static Filter equalTo(@NonNull FieldPath fieldPath, @NonNull Object value) {
-    return new UnqualifiedFieldFilter(
-        fieldPath.getInternalPath(), FieldFilter.Operator.EQUAL, value);
+  public static Filter equalTo(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter notEqualTo(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.NOT_EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter notEqualTo(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.NOT_EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter greaterThan(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.GREATER_THAN, value);
+  }
+
+  @NonNull
+  public static Filter greaterThan(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.GREATER_THAN, value);
+  }
+
+  @NonNull
+  public static Filter greaterThanOrEqualTo(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.GREATER_THAN_OR_EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter greaterThanOrEqualTo(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.GREATER_THAN_OR_EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter lessThan(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.LESS_THAN, value);
+  }
+
+  @NonNull
+  public static Filter lessThan(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.LESS_THAN, value);
+  }
+
+  @NonNull
+  public static Filter lessThanOrEqualTo(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.LESS_THAN_OR_EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter lessThanOrEqualTo(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.LESS_THAN_OR_EQUAL, value);
+  }
+
+  @NonNull
+  public static Filter arrayContains(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.ARRAY_CONTAINS, value);
+  }
+
+  @NonNull
+  public static Filter arrayContains(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.ARRAY_CONTAINS, value);
+  }
+
+  @NonNull
+  public static Filter arrayContainsAny(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.ARRAY_CONTAINS_ANY, value);
+  }
+
+  @NonNull
+  public static Filter arrayContainsAny(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.ARRAY_CONTAINS_ANY, value);
+  }
+
+  @NonNull
+  public static Filter in(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.IN, value);
+  }
+
+  @NonNull
+  public static Filter in(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.IN, value);
+  }
+
+  @NonNull
+  public static Filter notIn(@NonNull String field, @Nullable Object value) {
+    return createUnqualifiedFilter(
+        FieldPath.fromDotSeparatedPath(field), FieldFilter.Operator.NOT_IN, value);
+  }
+
+  @NonNull
+  public static Filter notIn(@NonNull FieldPath fieldPath, @Nullable Object value) {
+    return createUnqualifiedFilter(fieldPath, FieldFilter.Operator.NOT_IN, value);
   }
 
   @NonNull
@@ -33,4 +132,10 @@ public abstract class Filter {
   public abstract boolean matches(Document doc);
 
   public abstract String getCanonicalId();
+
+  @NonNull
+  private static Filter createUnqualifiedFilter(
+      @NonNull FieldPath fieldPath, @NonNull FieldFilter.Operator op, @Nullable Object value) {
+    return new UnqualifiedFieldFilter(fieldPath.getInternalPath(), op, value);
+  }
 }
